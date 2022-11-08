c2080d89d276306c1f22cf0c2324638bba7bf0ab
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-08 22:50:37 +0700

    setup server class

--- server/src/setupServer.ts
+++ server/src/setupServer.ts
@@ -1,5 +1,33 @@
-import {} from 'express';
+import {Application, json, urlencoded, Response, Request, NextFunction} from 'express';
+import http from 'http';
 
 export class ChattyServer {
+    private app:Application;
+
+    constructor(app: Application) {
+        this.app = app;
+    }
+
+    public start(): void {
+        this.securityMiddleware(this.app);
+        this.standardMiddleware(this.app);
+        this.routesMiddleware(this.app);
+        this.globalErrorHandler(this.app);
+        this.startServer(this.app);
+    }
+
+    public securityMiddleware(app: Application): void {}
+
+    public standardMiddleware(app: Application): void {}
+
+    public routesMiddleware(app: Application): void {}
+
+    public globalErrorHandler(app: Application): void {}
+
+    public startServer(app: Application): void {}
+
+    public createSocketIO(httpServer: http.Server): void {}
+
+    public startHttpServer(httpServer: http.Server): void {}
 
 }
