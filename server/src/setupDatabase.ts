902eb5d2bf31d7404c1d8d49e263ef68cb8d1b9b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-09 00:59:19 +0700

    environment config class 1.3

--- server/src/setupDatabase.ts
+++ server/src/setupDatabase.ts
@@ -1,18 +1,19 @@
 import mongoose from 'mongoose';
 
 export default () => {
-    const connect = () => {
-        mongoose.connect('mongodb://localhost:27017/chattyapp-backend')
-            .then(() => {
-                console.log('Successfully connected to database.');
-            })
-            .catch((error) => {
-                console.log('Error connecting to database', error);
-                return process.exit(1);
-            })
-    };
+  const connect = () => {
+    mongoose
+      .connect('mongodb://localhost:27017/chattyapp-backend')
+      .then(() => {
+        console.log('Successfully connected to database.');
+      })
+      .catch((error) => {
+        console.log('Error connecting to database', error);
+        return process.exit(1);
+      });
+  };
 
-    connect();
+  connect();
 
-    mongoose.connection.on('disconnected', connect);
+  mongoose.connection.on('disconnected', connect);
 };
