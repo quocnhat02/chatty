9c95b6d1ade625734ba14b69451ad1339db2c50c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-09 22:14:46 +0700

    global error handler 3

--- server/src/shared/globals/helpers/error-handler.ts
+++ server/src/shared/globals/helpers/error-handler.ts
@@ -30,8 +30,53 @@ export abstract class CustomError extends Error {
   }
 }
 
+export class JoiRequestValidationError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
 export class BadRequestError extends CustomError {
   statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class NotFoundError extends CustomError {
+  statusCode = HTTP_STATUS.NOT_FOUND;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class UnAuthorizedError extends CustomError {
+  statusCode = HTTP_STATUS.UNAUTHORIZED;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class FileTooLargeError extends CustomError {
+  statusCode = HTTP_STATUS.REQUEST_TOO_LONG;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class ServerError extends CustomError {
+  statusCode = HTTP_STATUS.SERVICE_UNAVAILABLE;
   status = 'error';
 
   constructor(message: string) {
