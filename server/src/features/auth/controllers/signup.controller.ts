cb5e2765b8f00bf15922075ba0174158df129475
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-10 20:26:35 +0700

    signup user method 1.1

---
+++ server/src/features/auth/controllers/signup.controller.ts
@@ -0,0 +1,11 @@
+import { ObjectId } from 'mongodb';
+import { Request, Response } from 'express';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemas/signup';
+
+export class SignUp {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { username, email, password, avatarColor, avatarImage } = req.body;
+  }
+}
