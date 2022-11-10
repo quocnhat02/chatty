d7f35724d3abe1dd282351e0bdd6823917056f81
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-10 21:32:26 +0700

    signup user method 1.2

--- server/src/features/auth/controllers/signup.controller.ts
+++ server/src/features/auth/controllers/signup.controller.ts
@@ -1,11 +1,19 @@
+import { authService } from '@service/db/auth.service';
+import { IAuthDocument } from '@auth/interfaces/auth.interface';
 import { ObjectId } from 'mongodb';
 import { Request, Response } from 'express';
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
 import { signupSchema } from '@auth/schemas/signup';
+import { BadRequestError } from '@global/helpers/error-handler';
 
 export class SignUp {
   @joiValidation(signupSchema)
   public async create(req: Request, res: Response): Promise<void> {
     const { username, email, password, avatarColor, avatarImage } = req.body;
+    const checkIUserExist: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
+
+    if (!checkIUserExist) {
+      throw new BadRequestError('Invalid credentials');
+    }
   }
 }
---
+++ server/src/shared/globals/helpers/helpers.ts
@@ -0,0 +1,14 @@
+export class Helpers {
+  static firstLetterUppercase(str: string): string {
+    const valueString = str.toLowerCase();
+
+    return valueString
+      .split(' ')
+      .map((value: string) => `${value.charAt(0).toUpperCase()}${value.slice(1).toLocaleLowerCase()}`)
+      .join(' ');
+  }
+
+  static lowerCase(str: string): string {
+    return str.toLowerCase();
+  }
+}
