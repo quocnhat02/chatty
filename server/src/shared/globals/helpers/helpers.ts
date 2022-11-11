e89c737d290160be9db37194065283e40cdb7cb1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-11 20:49:39 +0700

    signup user method 2.1

--- server/src/shared/globals/helpers/helpers.ts
+++ server/src/shared/globals/helpers/helpers.ts
@@ -11,4 +11,15 @@ export class Helpers {
   static lowerCase(str: string): string {
     return str.toLowerCase();
   }
+
+  static generateRandomIntegers(integerLength: nunmber): number {
+    const characters = '0123456789';
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < integerLength; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return parseInt(result, 10);
+  }
 }
