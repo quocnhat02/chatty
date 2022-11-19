18ac17dbb86327f2f8be5b9a85ad3699c7886564
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-19 22:49:36 +0700

    global error handler 1

---
+++ server/src/shared/global/helpers/error-handler.ts
@@ -0,0 +1,40 @@
+import HTTP_STATUS from 'http-status-codes';
+
+export interface IErrorResponse {
+  message: string;
+  statusCode: number;
+  status: string;
+  serializeErrors(): IError;
+}
+
+export interface IError {
+  message: string;
+  statusCode: number;
+  status: string;
+}
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+  abstract status: string;
+
+  constructor(message: string) {
+    super(message);
+  }
+
+  serializeErrors(): IError {
+    return {
+      message: this.message,
+      status: this.status,
+      statusCode: this.statusCode,
+    };
+  }
+}
+
+export class BadRequestError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
