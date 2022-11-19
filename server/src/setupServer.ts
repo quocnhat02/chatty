1dbf39a5834b4e083934aff9c8186d5023cc559a
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-19 21:07:51 +0700

    socketio setup 2

--- server/src/setupServer.ts
+++ server/src/setupServer.ts
@@ -13,6 +13,9 @@ import hpp from 'hpp';
 import compression from 'compression';
 import cookieSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
 import 'express-async-errors';
 import { config } from './config';
 
@@ -33,7 +36,7 @@ export class ChattyServer {
     this.startServer(this.app);
   }
 
-  public securityMiddleware(app: Application): void {
+  private securityMiddleware(app: Application): void {
     app.use(
       cookieSession({
         name: 'session',
@@ -54,30 +57,49 @@ export class ChattyServer {
     );
   }
 
-  public standardMiddleware(app: Application): void {
+  private standardMiddleware(app: Application): void {
     app.use(compression());
     app.use(json({ limit: '50mb' }));
     app.use(urlencoded({ limit: '50mb', extended: true }));
   }
 
-  public routesMiddleware(app: Application): void {}
+  private routesMiddleware(app: Application): void {}
 
-  public globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {}
 
-  public async startServer(app: Application): Promise<void> {
+  private async startServer(app: Application): Promise<void> {
     try {
       const httpServer: http.Server = new http.Server(app);
+      const socketIO: Server = await this.createSocketIO(httpServer);
       this.startHttpServer(httpServer);
+      this.socketIOConnection(socketIO);
     } catch (error) {
       console.log(error);
     }
   }
 
-  public createSocketIO(httpServer: http.Server): void {}
+  private async createSocketIO(httpServer: http.Server): Promise<Server> {
+    const io: Server = new Server(httpServer, {
+      cors: {
+        origin: config.CLIENT_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      },
+    });
 
-  public startHttpServer(httpServer: http.Server): void {
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    return io;
+  }
+
+  private startHttpServer(httpServer: http.Server): void {
+    console.log(`Server has started with process ${process.pid}`);
+
     httpServer.listen(SERVER_PORT, () => {
       console.log(`Server running on ${SERVER_PORT}`);
     });
   }
+
+  private socketIOConnection(io: Server): void {}
 }
