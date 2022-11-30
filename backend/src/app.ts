4dc7d2200c69dfe5d259fe05d37e2b49531a1b7d
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-11-30 16:25:50 +0700

    http server 2

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from 'express';
 import { ChattyServer } from './setupServer';
+import databaseConnection from './setupDatabase';
 
 class Application {
   public initialize(): void {
+    databaseConnection();
     const app: Express = express();
     const server: ChattyServer = new ChattyServer(app);
     server.start();
