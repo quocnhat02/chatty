695657f3786d422de6e3a5c2b7e25821e4b9814c
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-18 08:59:23 +0700

    http server

--- backend/src/setupServer.ts
+++ backend/src/setupServer.ts
@@ -15,6 +15,8 @@ import cookieSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
 
+const SERVER_PORT = 5000;
+
 export class ChattyServer {
   private app: Application;
 
@@ -62,9 +64,20 @@ export class ChattyServer {
 
   private globalErrorHandler(app: Application): void {}
 
-  private startServer(app: Application): void {}
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   private createSocketIO(httpServer: http.Server): void {}
 
-  private startHttpServer(httpServer: http.Server): void {}
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.log(`Server started on port ${SERVER_PORT}`);
+    });
+  }
 }
