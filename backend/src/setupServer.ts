9a51146c9480c976285ad3c7d9f8a7bb378687c1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-18 08:45:16 +0700

    standard middleware

--- backend/src/setupServer.ts
+++ backend/src/setupServer.ts
@@ -7,6 +7,13 @@ import {
   NextFunction,
 } from 'express';
 import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
 
 export class ChattyServer {
   private app: Application;
@@ -23,9 +30,33 @@ export class ChattyServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['test1', 'test2'],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      })
+    );
 
-  private standardMiddleware(app: Application): void {}
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ limit: '50mb', extended: true }));
+  }
 
   private routesMiddleware(app: Application): void {}
 
