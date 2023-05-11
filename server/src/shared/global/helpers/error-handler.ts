94e3d1cc68030568c91354f2fcd6770144474cdc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-12 00:41:19 +0700

    server setup error 2

--- server/src/shared/global/helpers/error-handler.ts
+++ server/src/shared/global/helpers/error-handler.ts
@@ -20,4 +20,66 @@ export abstract class CustomError extends Error {
   constructor(message: string) {
     super(message);
   }
+
+  serializeErrors(): IError {
+    return {
+      message: this.message,
+      status: this.status,
+      statusCode: this.statusCode,
+    };
+  }
+}
+
+export class JoiRequestValidationError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class BadRequestError extends CustomError {
+  statusCode = HTTP_STATUS.BAD_REQUEST;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class NotFoundError extends CustomError {
+  statusCode = HTTP_STATUS.NOT_FOUND;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class UnAuthorizedError extends CustomError {
+  statusCode = HTTP_STATUS.UNAUTHORIZED;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class FileTooLargeError extends CustomError {
+  statusCode = HTTP_STATUS.REQUEST_TOO_LONG;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class ServerError extends CustomError {
+  statusCode = HTTP_STATUS.INTERNAL_SERVER_ERROR;
+  status = 'error';
+
+  constructor(message: string) {
+    super(message);
+  }
 }
