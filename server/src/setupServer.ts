5ccf1de3cfcffed6b834d8026d677881d7ad61a0
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-11 21:47:59 +0700

    server setup 6

--- server/src/setupServer.ts
+++ server/src/setupServer.ts
@@ -7,4 +7,34 @@ import {
   NextFunction,
 } from 'express';
 
-export class AppServer {}
+import http from 'http';
+
+export class AppServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routesMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {}
+
+  private standardMiddleware(app: Application): void {}
+
+  private routesMiddleware(app: Application): void {}
+
+  private globalErrorHandler(app: Application): void {}
+
+  private startServer(app: Application): void {}
+
+  private createSocketIO(httpServer: http.Server): void {}
+
+  private startHttpServer(httpServer: http.Server): void {}
+}
