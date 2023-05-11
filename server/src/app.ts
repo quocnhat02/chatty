39a93c0e4352790e34a30d4e1b74efb2e9f38a92
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-11 22:28:37 +0700

    server setup 12

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+import express, { Express } from 'express';
+
+import { AppServer } from './setupServer';
+
+class Application {
+  public initialize(): void {
+    const app: Express = express();
+    const server: AppServer = new AppServer(app);
+    server.start();
+  }
+}
+
+const application: Application = new Application();
+
+application.initialize();
