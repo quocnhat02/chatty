fef280b5142db334c00abac0c77ba04b9cb5bdde
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-05-18 22:40:36 +0700

    redis 2

--- server/src/shared/service/redis/base.cache.ts
+++ server/src/shared/service/redis/base.cache.ts
@@ -1,6 +1,6 @@
-import { config } from '@root/config';
 import Logger from 'bunyan';
 import { createClient } from 'redis';
+import { config } from '@root/config';
 
 export type RedisClient = ReturnType<typeof createClient>;
 
