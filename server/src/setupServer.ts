51ff322f9b98f4df5df19fc4a853711dc1f8ae55
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-11 20:37:11 +0700

    setup http server 1

--- server/src/setupServer.ts
+++ server/src/setupServer.ts
@@ -15,6 +15,8 @@ import cookieSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
 
+const SERVER_PORT = 5000;
+
 export class AppServer {
   private app: Application;
 
@@ -70,9 +72,20 @@ export class AppServer {
 
   private globalErrorHandler(app: Application): void {}
 
-  private startServer(app: Application): void {}
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server();
+      this.startHttpServer(httpServer);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   private createSocketIO(httpServer: http.Server): void {}
 
-  private startHttpServer(httpServer: http.Server): void {}
+  private startHttpServer(httpServer: http.Server): void {
+    httpServer.listen(SERVER_PORT, () => {
+      console.log(`Server running on port ${SERVER_PORT}`);
+    });
+  }
 }
