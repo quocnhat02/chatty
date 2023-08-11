745a8bd3a6cb81e9f43f8520dbf787a6193261f9
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-08-11 16:39:31 +0700

    standard middleware

--- server/src/setupServer.ts
+++ server/src/setupServer.ts
@@ -6,8 +6,14 @@ import {
   Request,
   NextFunction,
 } from 'express';
-
 import http from 'http';
+import compression from 'compression';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
 
 export class AppServer {
   private app: Application;
@@ -24,9 +30,41 @@ export class AppServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: ['test1', 'test2'],
+        maxAge: 24 * 7 * 3600000,
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(
+      json({
+        limit: '50mb',
+      })
+    );
+    app.use(
+      urlencoded({
+        extended: true,
+        limit: '50mb',
+      })
+    );
+  }
 
   private routesMiddleware(app: Application): void {}
 
