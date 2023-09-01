266cae2461ab2cc9f5f2487ab2c62ea20686fc12
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-09-01 14:05:05 +0700

    setup environment 2

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -12,6 +12,7 @@ class Config {
 
   private readonly DEFAULT_DATABASE_URL =
     'mongodb://127.0.0.1:27017/chattyapp-backend';
+
   constructor() {
     this.DATABASE_URL = process.env.DATABASE_URL || this.DEFAULT_DATABASE_URL;
     this.JWT_TOKEN = process.env.JWT_TOKEN || 'jsonwebtoken';
